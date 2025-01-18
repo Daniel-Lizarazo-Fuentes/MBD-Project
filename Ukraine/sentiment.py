b2cb99be70f6a57c3f7bd011ce53f79d6b6ce737
Daniel-Lizarazo-Fuentes <daniel.lizarazo.fuentes@antipy.com>
2025-01-18 23:26:57 +0100

    [wip] batching and partiotion managment

--- Ukraine/sentiment.py
+++ Ukraine/sentiment.py
@@ -1,50 +1,24 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, lower, udf, when, year
-from pyspark.sql import functions as F
+from pyspark.sql.functions import col, count, lower, udf, when, year, substring
+from pyspark.storagelevel import StorageLevel
 from pyspark.sql.types import BooleanType
+from pyspark.sql import functions as F
 
 # -------------------------------------- Setup -----------------------------------------
 sc = SparkContext(appName="")
 sc.setLogLevel("ERROR")
 spark = SparkSession.builder.getOrCreate()
 
-# ------------------------------------- Load data  --------------------------------------
+base_path = "/user/s2551055/NewsData/"
 
-df = spark.read.parquet("/user/s2551055/NewsData_full/*/*.parquet").filter(
-    (col("published_date").isNotNull())
-    & (col("published_date") >= "2022-01-01")
-    & (col("language").isNotNull())
-    & (col("title").isNotNull())
-    # & (col("plain_text").isNotNull()) # This already crashes even at large numbers of executors
-    & (col("language").isin("en", "es", "ru", "ar"))
-)
-
-# ------------------------------------- Filtering ---------------------------------------
-
+# -------------------------------------- Keywords -----------------------------------------
 keywords_dict = {
     "en": ["ukraine", "russia"],
     "es": ["ucrania", "rusia"],
     "ru": ["украина", "россия"],
     "ar": ["أوكرانيا", "روسيا"],
 }
-
-filter_condition = None
-for keywords in keywords_dict.values():
-    condition = F.col("title").contains(keywords[0]) | F.col("title").contains(
-        keywords[1]
-    )
-    filter_condition = (
-        condition if filter_condition is None else filter_condition | condition
-    )
-
-df = df.filter(filter_condition)
-
-# --------------------------------- Printing results ----------------------------------
-
-# print("Rows after filtering: ",filtered_df.count())
-
-# --------------------------------- Sentiment analysis ----------------------------------
 
 sentiment_keywords = {
     "en": {
@@ -115,33 +89,83 @@ sentiment_keywords = {
         "negative": ["سيء", "فظيع", "حزين", "فشل", "خسارة", "سلبي", "غاضب"],
     },
 }
+# -------------------------------------- Functions -----------------------------------------
+filter_condition = None
+for keywords in keywords_dict.values():
+    condition = col("title").contains(keywords[0]) | col("title").contains(keywords[1])
+    filter_condition = (
+        condition if filter_condition is None else filter_condition | condition
+    )
 
-# All executors should have access to the keywords
-broadcast_keywords = spark.sparkContext.broadcast(sentiment_keywords)
 
 def classify_sentiment(text, lang):
     keywords = broadcast_keywords.value.get(lang, {})
-    positive_words = set(keywords.get('positive', []))
-    negative_words = set(keywords.get('negative', []))
+    positive_words = set(keywords.get("positive", []))
+    negative_words = set(keywords.get("negative", []))
 
-    # Tokenize the text and count occurrences of positive and negative words
-    words = text.split()  # Use split to break the text into words
+    words = text.split() 
     positive_count = sum(1 for word in words if word.lower() in positive_words)
     negative_count = sum(1 for word in words if word.lower() in negative_words)
 
     return positive_count >= negative_count
 
 
-classify_sentiment_udf = F.udf(lambda text, lang: classify_sentiment(text, lang), BooleanType())
-
-df_with_sentiment = df.withColumn("sentiment_positive", classify_sentiment_udf(F.col("title"), F.col("language")))
-df_with_sentiment = df_with_sentiment.withColumn("year", year(F.col("published_date")))
-
-sentiment_counts = (
-    df_with_sentiment.groupBy("year", "language", "sentiment_positive")
-    .count()
-    .orderBy("year", "language")
+classify_sentiment_udf = udf(
+    lambda text, lang: classify_sentiment(text, lang), BooleanType()
 )
 
-# --------------------------------- Printing results ----------------------------------
-sentiment_counts.show()
+# ------------------------------------- Filtering --------------------------------------
+df = spark.read.parquet("/user/s2551055/NewsData/*/*.parquet").filter(
+    (col("published_date").isNotNull())
+    & (col("published_date") >= "2022-01-01")
+    & (col("language").isNotNull())
+    & (col("title").isNotNull())
+    # & (col("plain_text").isNotNull()) # This already crashes even at large numbers of executors
+    & (col("language").isin("en", "es", "ru", "ar"))
+)
+
+df = df.filter(filter_condition)
+df = df.withColumn("year", year(col("published_date")))
+df = df.withColumn("plain_text", substring("plain_text", 0, 500))
+df = df.withColumn("plain_text", lower(col("plain_text")))
+size = df.count()
+print("Rows before filtering: ", size)
+
+# --------------------------------- Sentiment analysis ----------------------------------
+
+# All executors should have access to all keywords
+broadcast_keywords = spark.sparkContext.broadcast(sentiment_keywords)
+
+result = spark.createDataFrame(
+    [], "year STRING, language STRING, sentiment_positive BOOLEAN, count LONG"
+)
+
+df = df.repartition(20, "year", "language") 
+
+chunk_size = 5000
+offset = 0
+
+while offset < size:
+    chunk_df = df.limit(offset + chunk_size).subtract(df.limit(offset))
+
+    df_with_sentiment = chunk_df.withColumn(
+        "sentiment_positive", classify_sentiment_udf(col("plain_text"), col("language"))
+    )
+
+    sentiment_counts = df_with_sentiment.groupBy(
+        "year", "language", "sentiment_positive"
+    ).count()
+
+    result = result.union(sentiment_counts)
+
+    offset += chunk_size
+
+
+final_result = (
+    result.groupBy("year", "language", "sentiment_positive")
+    .agg(F.sum("count").alias("count"))
+    .orderBy("language", "year", "sentiment_positive")
+)
+
+
+final_result.show(100)
